2e15684cdd4ef9764f9bac8a6d7b7a51c57fdb35
dchenk <dcherchenko@gmail.com>
2019-06-02 11:09:55 -0700

    JS cleanups

--- js/ctt.js
+++ js/ctt.js
@@ -1,42 +1,34 @@
-(function () {
-	function ShowSelection()
-	{
-		var selectedText = tinyMCE.activeEditor.selection.getContent( {format : "text"} );
-		return selectedText;
-	}
-	tinymce.create('tinymce.plugins.ctt', {
-		init: function (ed, url) {
-			ed.addButton('ctt', {
-				title: 'Click To Tweet WordPress Plugin',
-				image: url.replace("/js", "") + '/images/editor-ticon.png',
-				onclick: function () {
-					var seltxt =  ShowSelection();
-					var pre_txt =  encodeURIComponent(seltxt).replace("%A0", "%20");
-					var permalink = jQuery('#sample-permalink a').attr('href');
-					jQuery(document).ready(function ($) {
+(function() {
+	const showSelection = () => tinyMCE.activeEditor.selection.getContent({ format: "text" });
+	tinymce.create("tinymce.plugins.ctt", {
+		init: function(ed, url) {
+			ed.addButton("ctt", {
+				title: "Click To Tweet Plugin",
+				image: url.replace("/js", "") + "/images/editor-ticon.png",
+				onclick: function() {
+					const seltxt = showSelection();
+					const pre_txt = encodeURIComponent(seltxt).replace("%A0", "%20");
+					const permalink = jQuery("#sample-permalink a").attr("href");
+					jQuery(document).ready(function($) {
 						tinymce.activeEditor.windowManager.open({
-							title: "Click To Tweet WordPress Plugin",
+							title: "Click To Tweet Plugin",
 							width: 1030,
 							height: 600,
-							classes: 'myAwesomeClass-panel',
-							url: ajaxurl + '?action=ctt_show_dialog&pretext='+pre_txt+'&permalink='+permalink,
+							classes: "myAwesomeClass-panel",
+							url: `${ajaxurl}?action=ctt_show_dialog&pretext=${pre_txt}&permalink${permalink}`,
 						});
 					});
 				}
 			});
 		},
-		createControl: function (n, cm) {
-			return null;
-		},
-		getInfo: function () {
-			return {
-				longname: "Click To Tweet WordPress Plugin",
-				author: 'ClickToTweet.com',
-				authorurl: 'http://ctt.ec/',
-				infourl: 'http://ctt.ec/',
-				version: "2.0.10"
-			};
-		}
+		createControl: (n, cm) => null,
+		getInfo: () => ({
+			longname: "Click To Tweet Plugin",
+			author: "ClickToTweet.com",
+			authorurl: "http://ctt.ec/",
+			infourl: "http://ctt.ec/",
+			version: "2.0.10"
+		}),
 	});
-	tinymce.PluginManager.add('ctt', tinymce.plugins.ctt);
+	tinymce.PluginManager.add("ctt", tinymce.plugins.ctt);
 })();
